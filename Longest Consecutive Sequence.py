7737d1349576b133977c40d5621724f2f4691360
Tej410 <tejdeep20012013@gmail.com>
2024-01-30 18:05:53 -0500

    add longest consecutive sequence

---
+++ Longest Consecutive Sequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        nums.sort()
+        max_len = 0
+        curr_len = 0
+        for i in range(0,len(nums)-1):
+            if nums[i+1] == nums[i]:
+                continue
+            if nums[i+1] == nums[i] + 1:
+                curr_len += 1
+            else:
+                curr_len = 0
+            max_len = max(max_len,curr_len)
+        return max_len+1
