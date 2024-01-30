3c93383c1cd91da936522dfd5ad3d2a3a33291f7
Tej410 <tejdeep20012013@gmail.com>
2024-01-29 23:23:38 -0500

    product of array except self added

---
+++ Product of Array Except Self.py
@@ -0,0 +1,21 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # output = []
+        # for i in range(len(nums)):
+        #     prod = 1
+        #     for j in range(len(nums)):
+        #         if i!=j:
+        #             prod*=nums[j]
+        #     output.append(prod)
+        # return output
+        output=[1]*len(nums)
+        prefix=1
+        for i in range(len(nums)):
+            output[i] = prefix
+            prefix *= nums[i]
+        postfix = nums[len(nums)-1]
+        for i in range(len(nums)-2,-1,-1):
+            output[i] *= postfix
+            postfix *= nums[i]
+        return output
+            
