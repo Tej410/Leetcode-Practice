e34b1f033b8ee675d434f10830eeed00ad337b16
Tej410 <tejdeep20012013@gmail.com>
2024-02-01 17:21:51 -0500

    add 3Sum

---
+++ 3Sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = set()
+        nums.sort()
+        for i in range(len(nums)-2):
+            j = i+1
+            k = len(nums)-1
+            while j<k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == 0:
+                    result.add((nums[i],nums[j],nums[k]))
+                    j+=1
+                elif sum < 0:
+                    j+=1
+                else:
+                    k-=1
+        return list(result)
+         
+        
